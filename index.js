6d52d8d592bec7ba65f0cc98962ecd4a64782d67
Alice Freda <alicefreda@gmail.com>
2017-11-12 20:08:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
-BoardMember.prototype.veto = function() {
-  return ("No, I must disagree");
+this.veto = function(){
+  return 'No, I must disagree'
 }
-BoardMember.prototype.approve = function() {
-  return ("You can do that!");
+this.approve = function(){
+  return 'You can do that!'
 }
-BoardMember.prototype.doCharity = function() {
-  return ("I like to help people.");
+this.doCharity = function(){
+  return "I like to help people."
 }
-BoardMember.prototype.releasePressStatement = function() {
-  return ("You will see great things from Scuber.");
+
+this.releasePressStatement = function(){
+  return "You will see great things from Scuber."
 }
-BoardMember.prototype.sayHi = function(){
-  return (`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
+
+this.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
+
